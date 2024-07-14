1bcf8598b8d2ca85cddcdc8799dcba0878d598c5
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-14 07:57:06 +0530

    add SearchAnEle.java

---
+++ LinkedList/Learn-1D-LL/SearchAnEle.java
@@ -0,0 +1,65 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+    }
+}
+
+class LinkedList {
+    public static Node createLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    public static void displayLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+    public static boolean searchAnEle(Node head, int target) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == target) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public static int lengthOfLL(Node head) {
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+}
+
+public class SearchAnEle {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Node head = LinkedList.createLL(arr);
+        int target = 4;
+        boolean isFound = LinkedList.searchAnEle(head, target);
+        System.out.println("Element present or not? : " + isFound);
+    }
+}
